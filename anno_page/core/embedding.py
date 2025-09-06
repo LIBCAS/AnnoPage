3136e54c92bab2a0428299276b1c7f09e3083061
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2025-09-06 13:00:42 +0200

    Update ElementEmbeddings definition and methods and add jsonlines output

--- anno_page/core/embedding.py
+++ anno_page/core/embedding.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List, Iterator, Any
 from pydantic import BaseModel, RootModel
 
 
@@ -19,5 +19,29 @@ class Element(BaseModel):
     embedding: list[float]
 
 
-class ElementEmbeddings(RootModel[list[Element]]):
-    pass
+class ElementEmbeddings(RootModel[List[Element]]):
+    def __init__(self, items: Optional[List[Element]] = None):
+        super().__init__(items or [])
+
+    def __iter__(self) -> Iterator[Element]:
+        return iter(self.root)
+    def __len__(self) -> int:
+        return len(self.root)
+    def __getitem__(self, i: int) -> Element:
+        return self.root[i]
+    def __setitem__(self, i: int, e: Element) -> None:
+        self.root[i] = e
+
+    def append(self, e: Element) -> None:
+        self.root.append(e)
+    def extend(self, l) -> None:
+        self.root.extend(l)
+    def insert(self, i: int, e: Element) -> None:
+        self.root.insert(i, e)
+    def pop(self, i: int = -1) -> Element:
+        return self.root.pop(i)
+    def clear(self) -> None:
+        self.root.clear()
+
+    def model_dump_jsonlines(self, **kwargs: Any) -> str:
+        return "\n".join(e.model_dump_json(**kwargs) for e in self.root)
