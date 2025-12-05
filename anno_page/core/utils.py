fb031f71c8ba539003e8a8e7899cc74aaa9b1428
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2025-12-05 11:43:35 +0100

    Add utils -- functions for composing paths and getting lists for configs

---
+++ anno_page/core/utils.py
@@ -0,0 +1,24 @@
+import os
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def compose_path(file_path, reference_path):
+    if reference_path and not os.path.isabs(file_path):
+        file_path = os.path.join(reference_path, file_path)
+    return file_path
+
+
+def config_get_list(config, key, fallback=None):
+    if key not in config:
+        return fallback
+
+    try:
+        value = json.loads(config[key])
+    except json.decoder.JSONDecodeError as e:
+        logger.info(f'Failed to parse list from config key "{key}", returning fallback {fallback}:\n{e}')
+        return fallback
+    else:
+        return value
