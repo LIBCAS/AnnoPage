442b9e9c74f58093b61e58b9e4f6de1db552e50e
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2025-12-08 19:04:08 +0100

    Add basic AnnoPageAPI configuration

--- api/api.py
+++ api/api.py
@@ -1,4 +1,19 @@
+import os
 from doc_api.run import main
 
 if __name__ == "__main__":
+    config = {
+        "SERVER_NAME": "AnnoPageAPI",
+        "APP_VERSION": "1.0.0",
+        "APP_HOST": "0.0.0.0",
+        "APP_PORT": "8000",
+        "PRODUCTION": "False",
+        "KEY_PREFIX": "annopage",
+        "BASE_DIR": "./annopage_api_data",
+    }
+
+    for key, value in config.items():
+        if not os.environ.get(key, None):
+            os.environ[key] = value
+
     main()
