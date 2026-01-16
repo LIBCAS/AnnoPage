117f1c3b7c4a9255be30876e14f778459e45b5f4
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2026-01-16 17:54:33 +0100

    Refactor routes engines initialization and add siglip engine

--- api/routes.py
+++ api/routes.py
@@ -4,12 +4,12 @@ from fastapi import Depends
 from typing import Optional
 
 from doc_api.api.routes import root_router
-from doc_api.api.schemas import base_objects
+from doc_api.api.schemas import base_objects, responses
 from doc_api.api.authentication import require_api_key
 from doc_api.db import model
 
 from anno_page.engines.translation import TranslationEngine
-from anno_page.engines.embedding import ClipTextEmbeddingEngine
+from anno_page.engines.embedding import HuggingfaceTextEmbeddingEngine
 from anno_page.engines.captioning import PromptBuilderEngine
 
 
@@ -19,26 +19,71 @@ def dict_to_config_section(data: dict):
     config[section_name] = data
     return config[section_name]
 
-translation_engine_model_name = "Helsinki-NLP/opus-mt-cs-en"
-translation_engine_config = dict_to_config_section(data={
-                                                       "TOKENIZER": translation_engine_model_name,
-                                                       "MODEL": translation_engine_model_name
-                                                   })
-translation_engine = TranslationEngine(config=translation_engine_config,
-                                       device="cpu",
-                                       config_path="")
 
-text_embedding_engine_config = dict_to_config_section(data={
-                                                         "MODEL": "clip-ViT-L-14",
-                                                         "PRECISION": "float32",
-                                                         "DECIMAL_PLACES": "6"
-                                                      })
-text_embedding_engine = ClipTextEmbeddingEngine(config=text_embedding_engine_config,
-                                                 device="cpu",
-                                                 config_path="")
+class LoadedEngines:
+    translation_engine: TranslationEngine | None = None
+    clip_text_embedding_engine: HuggingfaceTextEmbeddingEngine | None = None
+    siglip_text_embedding_engine: HuggingfaceTextEmbeddingEngine | None = None
+    prompt_builder_engine: PromptBuilderEngine | None = None
 
-prompt_builder_engine = PromptBuilderEngine()
 
+def setup():
+    global loaded_engines
+    if loaded_engines is None:
+        loaded_engines = LoadedEngines()
+
+    loaded_engines.translation_engine = load_translation_engine()
+    loaded_engines.clip_text_embedding_engine = load_clip_text_embedding_engine()
+    loaded_engines.siglip_text_embedding_engine = load_siglip_text_embedding_engine()
+    loaded_engines.prompt_builder_engine = load_prompt_builder_engine()
+
+
+def load_translation_engine() -> TranslationEngine:
+    translation_engine_model_name = "Helsinki-NLP/opus-mt-cs-en"
+    translation_engine_config = dict_to_config_section(data={
+                                                           "TOKENIZER": translation_engine_model_name,
+                                                           "MODEL": translation_engine_model_name
+                                                       })
+    translation_engine = TranslationEngine(config=translation_engine_config,
+                                           device="cpu",
+                                           config_path="")
+
+    return translation_engine
+
+
+def load_clip_text_embedding_engine() -> HuggingfaceTextEmbeddingEngine:
+    clip_text_embedding_engine_config = dict_to_config_section(data={
+                                                                   "MODEL": "openai/clip-vit-large-patch14",
+                                                                   "PRECISION": "float16",
+                                                                   "DECIMAL_PLACES": "6"
+                                                               })
+    clip_text_embedding_engine = HuggingfaceTextEmbeddingEngine(config=clip_text_embedding_engine_config,
+                                                                device="cpu",
+                                                                config_path="")
+
+    return clip_text_embedding_engine
+
+
+def load_siglip_text_embedding_engine() -> HuggingfaceTextEmbeddingEngine:
+    siglip_text_embedding_engine_config = dict_to_config_section(data={
+                                                                     "MODEL": "google/siglip2-large-patch16-512",
+                                                                     "PRECISION": "float16",
+                                                                     "DECIMAL_PLACES": "6"
+                                                                 })
+    siglip_text_embedding_engine = HuggingfaceTextEmbeddingEngine(config=siglip_text_embedding_engine_config,
+                                                                  device="cpu",
+                                                                  config_path="")
+
+    return siglip_text_embedding_engine
+
+
+def load_prompt_builder_engine() -> PromptBuilderEngine:
+    prompt_builder_engine = PromptBuilderEngine()
+    return prompt_builder_engine
+
+
+loaded_engines = LoadedEngines()
+setup()
 
 @root_router.get(
     "/v1/text/translation",
@@ -51,22 +96,44 @@ prompt_builder_engine = PromptBuilderEngine()
 async def text_translation(
     text: str,
     key: model.Key = Depends(require_api_key(base_objects.KeyRole.READONLY, base_objects.KeyRole.USER))):
-    output = translation_engine.process(text)[0]
-    return output
+    if loaded_engines.translation_engine is not None:
+        result = loaded_engines.translation_engine.process(text)[0]
+    else:
+        result = responses.DocAPIResponseServerError(status=fastapi.status.HTTP_503_SERVICE_UNAVAILABLE, code='INTERNAL_ERROR', detail="Translation engine is currently not available.")
+    return result
 
 
 @root_router.get(
-    "/v1/text/embedding",
+    "/v1/text/embedding/clip",
     summary="Text Embedding",
     tags=["User"],
     openapi_extra={"x-order": 667},
     description="Converts text to embedding.",
     status_code=fastapi.status.HTTP_200_OK)
-async def text_embedding(
+async def text_embedding_clip(
         text: str,
         key: model.Key = Depends(require_api_key(base_objects.KeyRole.READONLY, base_objects.KeyRole.USER))):
-    embeddings = text_embedding_engine.process(text)[0]
-    return embeddings
+    if loaded_engines.clip_text_embedding_engine is not None:
+        result = loaded_engines.clip_text_embedding_engine.process(text)[0]
+    else:
+        result = responses.DocAPIResponseServerError(status=fastapi.status.HTTP_503_SERVICE_UNAVAILABLE, code='INTERNAL_ERROR', detail="Clip text embedding engine is currently not available.")
+    return result
+
+@root_router.get(
+    "/v1/text/embedding/siglip",
+    summary="Text Embedding",
+    tags=["User"],
+    openapi_extra={"x-order": 667},
+    description="Converts text to embedding.",
+    status_code=fastapi.status.HTTP_200_OK)
+async def text_embedding_siglip(
+        text: str,
+        key: model.Key = Depends(require_api_key(base_objects.KeyRole.READONLY, base_objects.KeyRole.USER))):
+    if loaded_engines.siglip_text_embedding_engine is not None:
+        result = loaded_engines.siglip_text_embedding_engine.process(text)[0]
+    else:
+        result = responses.DocAPIResponseServerError(status=fastapi.status.HTTP_503_SERVICE_UNAVAILABLE, code='INTERNAL_ERROR', detail="Siglip text embedding engine is currently not available.")
+    return result
 
 
 @root_router.post(
@@ -82,8 +149,11 @@ async def prompt_evaluation(
         title: Optional[str] = None,
         metadata: Optional[dict[str, str]] = None,
         key: model.Key = Depends(require_api_key(base_objects.KeyRole.READONLY, base_objects.KeyRole.USER))):
-    output = prompt_builder_engine.process(prompt=prompt,
-                                           category=category,
-                                           title=title,
-                                           metadata=metadata)
-    return output
+    if loaded_engines.prompt_builder_engine is not None:
+        result = loaded_engines.prompt_builder_engine.process(prompt=prompt,
+                                                              category=category,
+                                                              title=title,
+                                                              metadata=metadata)
+    else:
+        result = responses.DocAPIResponseServerError(status=fastapi.status.HTTP_503_SERVICE_UNAVAILABLE, code='INTERNAL_ERROR', detail="Prompt builder engine is currently not available.")
+    return result
