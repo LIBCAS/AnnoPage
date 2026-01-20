369fb5712e6843c22163db7547019e11e9a77020
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2026-01-20 09:22:39 +0100

    Add optional parameter to make string items in a list lowercase when obtaining list from config file

--- anno_page/core/utils.py
+++ anno_page/core/utils.py
@@ -11,7 +11,7 @@ def compose_path(file_path, reference_path):
     return file_path
 
 
-def config_get_list(config, key, fallback=None):
+def config_get_list(config, key, fallback=None, make_lowercase=False):
     if key not in config:
         return fallback
 
@@ -20,5 +20,8 @@ def config_get_list(config, key, fallback=None):
     except json.decoder.JSONDecodeError as e:
         logger.info(f'Failed to parse list from config key "{key}", returning fallback {fallback}:\n{e}')
         return fallback
-    else:
-        return value
+
+    if make_lowercase:
+        value = [str(item).lower() if isinstance(item, str) else item for item in value]
+
+    return value
