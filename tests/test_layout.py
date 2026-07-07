0533cb473bc21f5585062f138313cf704edc0fe0
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2026-07-07 12:36:55 +0200

    Add test for ALTO image metadata including used AI models

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -297,3 +297,101 @@ def test_alto_image_with_basic_metadata_and_text_lines_tagrefs():
     expected_alto = load_xml(os.path.join(resources_dir, "alto_image_with_basic_metadata_and_text_lines_tagrefs.xml"))
 
     assert_xml_equal(generated_alto, expected_alto)
+
+
+def test_alto_image_with_basic_metadata_and_text_lines_tagrefs_ai_models():
+    setup()
+
+    image_top = 20
+    image_left = 10
+    image_bottom = 100
+    image_right = 80
+    image_polygon = np.array([[image_left, image_top],
+                              [image_right, image_top],
+                              [image_right, image_bottom],
+                              [image_left, image_bottom]])
+    image_region = AnnoPageRegionLayout(id="image_region", polygon=image_polygon, category="Image", detection_confidence=0.9)
+    image_region.graphical_metadata = GraphicalObjectMetadata(
+        tag_id="image_001",
+        mods_id="MODS_PICT_0001",
+        description={
+            Language.ENGLISH: "This is a description of an image",
+            Language.CZECH: "Toto je popis obrázku"
+        },
+        caption={
+            Language.ENGLISH: "This is a caption of an image",
+            Language.CZECH: "Toto je titulek obrázku"
+        },
+        topics={
+            Language.ENGLISH: ["topic1", "topic2"],
+            Language.CZECH: ["topic1", "topic2"]
+        },
+        color={
+            Language.ENGLISH: "grayscale",
+            Language.CZECH: "černobílý"
+        },
+        title="This is line no. 2",
+        used_ai_models={
+            "element-detection": "annopage-yolo-1.0",
+        }
+    )
+
+    text_top = 120
+    text_left = 10
+    text_bottom = 200
+    text_right = 200
+
+    text_polygon = np.array([[text_left, text_top],
+                             [text_right, text_top],
+                             [text_right, text_bottom],
+                             [text_left, text_bottom]])
+
+    text_region = RegionLayout(id="text_region", polygon=text_polygon, category="text")
+
+    line_left = 10
+    line_right = 200
+
+    line_tops = [120, 140, 160, 180]
+    line_bottoms = [140, 160, 180, 200]
+    baseline_tops = [bottom - 5 for bottom in line_bottoms]
+
+    line_polygons = [np.array([[line_left, top],
+                               [line_right, top],
+                               [line_right, bottom],
+                               [line_left, bottom]]) for top, bottom in zip(line_tops, line_bottoms)]
+
+    baselines = [np.array([[line_left, baseline_top],
+                           [line_right, baseline_top]]) for baseline_top in baseline_tops]
+
+    line1 = TextLine(id="line1", polygon=line_polygons[0], baseline=baselines[0], transcription="This is line no. 1.")
+    line2 = TextLine(id="line2", polygon=line_polygons[1], baseline=baselines[1], transcription="This is line no. 2.")
+    line3 = TextLine(id="line3", polygon=line_polygons[2], baseline=baselines[2], transcription="This is line no. 3.")
+    line4 = TextLine(id="line4", polygon=line_polygons[3], baseline=baselines[3], transcription="This is line no. 4.")
+
+    text_region.lines.append(line1)
+    text_region.lines.append(line2)
+    text_region.lines.append(line3)
+    text_region.lines.append(line4)
+
+    image_region.graphical_metadata.caption_lines_metadata = RelatedLinesMetadata(
+        tag_id="fc.image_001",
+        mods_id="MODS_PICT_0001_CAPTION_0001",
+        lines=[line2],
+        relation=LineRelation.CAPTION,
+        description=line2.transcription,
+        title={
+            Language.ENGLISH: line2.transcription
+        }
+    )
+
+    page_layout = PageLayout(id="test_page", page_size=(297, 210))
+    page_layout.regions.append(image_region)
+    page_layout.regions.append(text_region)
+
+    set_handlers(page_layout)
+    generated_alto = page_layout.to_altoxml_string(version=ALTOVersion.ALTO_v4_4)
+
+    resources_dir = os.path.join(os.path.dirname(__file__), "resources")
+    expected_alto = load_xml(os.path.join(resources_dir, "alto_image_with_basic_metadata_and_text_lines_tagrefs_ai_models.xml"))
+
+    assert_xml_equal(generated_alto, expected_alto)
